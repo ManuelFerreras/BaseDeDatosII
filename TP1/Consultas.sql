52ff19ee145a6fde7d313a321ca0342cac0742f7
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2024-09-05 16:47:33 -0300

    Update Consultas.sql

--- TP1/Consultas.sql
+++ TP1/Consultas.sql
@@ -3,24 +3,25 @@ FROM carrera c
 JOIN inscripcion_carrera ic ON c.id_carrera = ic.id_carrera
 GROUP BY c.nombre_carrera;
 
-SELECT pm.anio_cursada, pm.cuatrimestre, c.nombre_carrera, COUNT(ic.id_alumno) AS cantidad_alumnos
-FROM plan_de_materia pm
-JOIN carrera c ON pm.id_carrera = c.id_carrera
+SELECT p.anio_cursada, p.cuatrimestre, c.nombre_carrera, COUNT(ic.id_alumno) AS cantidad_alumnos
+FROM plan_de_materia p
+JOIN carrera c ON p.id_carrera = c.id_carrera
 JOIN inscripcion_carrera ic ON c.id_carrera = ic.id_carrera
-GROUP BY pm.anio_cursada, pm.cuatrimestre, c.nombre_carrera;
+GROUP BY p.anio_cursada, p.cuatrimestre, c.nombre_carrera;
 
-SELECT pm.anio_cursada, pm.cuatrimestre, c.nombre_carrera, COUNT(ic.id_alumno) AS cantidad_activos
-FROM plan_de_materia pm
-JOIN carrera c ON pm.id_carrera = c.id_carrera
+SELECT p.anio_cursada, p.cuatrimestre, c.nombre_carrera, COUNT(DISTINCT im.id_alumno) AS cantidad_activos
+FROM plan_de_materia p
+JOIN carrera c ON p.id_carrera = c.id_carrera
 JOIN inscripcion_carrera ic ON c.id_carrera = ic.id_carrera
 JOIN inscripcion_materia im ON ic.id_alumno = im.id_alumno
 WHERE im.estado = 'Cursando'
-GROUP BY pm.anio_cursada, pm.cuatrimestre, c.nombre_carrera;
+GROUP BY p.anio_cursada, p.cuatrimestre, c.nombre_carrera;
 
 SELECT m.nombre_materia, a.nombre, a.apellido
 FROM inscripcion_materia im
-JOIN materiaXplanXcomision mxc ON im.id_materiaXplanXcomision = mxc.id_materiaXplanXcomision
-JOIN materia m ON mxc.id_materiaXplanXcomision = m.id_materia
+JOIN materiaXplanXcomision mpc ON im.id_materiaXplanXcomision = mpc.id_materiaXplanXcomision
+JOIN materiaXplan mp ON mpc.id_materiaXplan = mp.id_materiaXplan
+JOIN materia m ON mp.id_materia = m.id_materia
 JOIN alumno a ON im.id_alumno = a.id_alumno
 WHERE im.estado = 'Cursando';
 
@@ -28,14 +29,18 @@ SELECT m.nombre_materia,
   SUM(CASE WHEN im.estado = 'Regular' THEN 1 ELSE 0 END) AS cantidad_regulares,
   SUM(CASE WHEN im.estado = 'Libre' THEN 1 ELSE 0 END) AS cantidad_libres
 FROM inscripcion_materia im
-JOIN materiaXplanXcomision mxc ON im.id_materiaXplanXcomision = mxc.id_materiaXplanXcomision
-JOIN materia m ON mxc.id_materiaXplanXcomision = m.id_materia
+JOIN materiaXplanXcomision mpc ON im.id_materiaXplanXcomision = mpc.id_materiaXplanXcomision
+JOIN materiaXplan mp ON mpc.id_materiaXplan = mp.id_materiaXplan
+JOIN materia m ON mp.id_materia = m.id_materia
 GROUP BY m.nombre_materia;
+
 
 SELECT m.nombre_materia,
   COUNT(ie.id_inscripcion_examen) AS cantidad_examenes_rendidos,
   SUM(CASE WHEN ie.nota > 4 THEN 1 ELSE 0 END) AS cantidad_aprobados
 FROM inscripcion_examen ie
-JOIN parcial p ON ie.id_parcial = p.id_parcial
-JOIN materia m ON p.id_materia = m.id_materia
+JOIN inscripcion_materia im ON ie.id_inscripcion_examen = im.id_insc_materia
+JOIN materiaXplanXcomision mpc ON im.id_materiaXplanXcomision = mpc.id_materiaXplanXcomision
+JOIN materiaXplan mp ON mpc.id_materiaXplan = mp.id_materiaXplan
+JOIN materia m ON mp.id_materia = m.id_materia
 GROUP BY m.nombre_materia;
